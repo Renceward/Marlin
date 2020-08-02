9e08872aa91d78679ed9a0b491615986caa389ad
Lawrence Sweet <git@larrysweet.com>
2020-08-02 14:32:09 -0500

    Second Major commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HolidaysComponent } from './holidays/holidays.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { LocationComponent } from './location/location.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TitleComponent
+    TitleComponent,
+    NavbarComponent,
+    HolidaysComponent,
+    ScheduleComponent,
+    LocationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
